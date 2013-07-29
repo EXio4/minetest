042c8614ab22513df2e04cac1ea54d8318aa61a6
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2013-07-28 22:00:23 -0300

    Cosmetic fixes

--- client/shaders/bumpmaps_solids/opengl_fragment.glsl
+++ client/shaders/bumpmaps_solids/opengl_fragment.glsl
@@ -9,7 +9,7 @@ varying vec3 vPosition;
 
 varying vec3 viewVec;
 
-void main (void)
+void main(void)
 {
 	vec4 col = texture2D(myTexture, vec2(gl_TexCoord[0]));
 	float alpha = col.a;
@@ -21,16 +21,14 @@ void main (void)
 	vec3 R = reflect(-vVec, bump);
 	vec3 lVec = normalize(vec3(0.0, -0.4, 0.5));
 	float diffuse = max(dot(lVec, bump), 0.0);
-  
+
 	vec3 color = diffuse * texture2D(myTexture, gl_TexCoord[0].st).rgb;
-	
-	
+
 	float specular = pow(clamp(dot(R, lVec), 0.0, 1.0),1.0);
-	vec4 vSpecular = 0.2*specular * diffuse;	
-    color += vSpecular;
- 
-  
-	col = vec4(color.r, color.g, color.b, alpha);
+	vec4 vSpecular = 0.2*specular * diffuse;
+	color += vSpecular;
+
+	// apply lighting
 	col *= gl_Color;
 	col = col * col; // SRGB -> Linear
 	col *= 1.8;
@@ -38,9 +36,12 @@ void main (void)
 	col.g = 1.0 - exp(1.0 - col.g) / exp(1.0);
 	col.b = 1.0 - exp(1.0 - col.b) / exp(1.0);
 	col = sqrt(col); // Linear -> SRGB
+
+	// apply "fog"
 	if(fogDistance != 0.0){
 		float d = max(0.0, min(vPosition.z / fogDistance * 1.5 - 0.6, 1.0));
 		col = mix(col, skyBgColor, d);
 	}
-    gl_FragColor = vec4(col.r, col.g, col.b, alpha);   
+
+	gl_FragColor = vec4(col.r, col.g, col.b, alpha);
 }
--- client/shaders/bumpmaps_solids/opengl_vertex.glsl
+++ client/shaders/bumpmaps_solids/opengl_vertex.glsl
@@ -12,25 +12,25 @@ void main(void)
 	gl_Position = mWorldViewProj * gl_Vertex;
 
 	vPosition = (mWorldViewProj * gl_Vertex).xyz;
-	
-	vec3 tangent; 
-	vec3 binormal; 
 
-	vec3 c1 = cross( gl_Normal, vec3(0.0, 0.0, 1.0) ); 
-	vec3 c2 = cross( gl_Normal, vec3(0.0, 1.0, 0.0) ); 
+	vec3 tangent;
+	vec3 binormal;
+
+	vec3 c1 = cross( gl_Normal, vec3(0.0, 0.0, 1.0) );
+	vec3 c2 = cross( gl_Normal, vec3(0.0, 1.0, 0.0) );
 
 	if( length(c1)>length(c2) )
 	{
-		tangent = c1;	
+		tangent = c1;
 	}
 	else
 	{
-		tangent = c2;	
+		tangent = c2;
 	}
 
 	tangent = normalize(tangent);
 
-//binormal = cross(gl_Normal, tangent); 
+//binormal = cross(gl_Normal, tangent);
 //binormal = normalize(binormal);
 
 	vec4 color;
@@ -76,7 +76,7 @@ void main(void)
 	gl_FrontColor = gl_BackColor = color;
 
 	gl_TexCoord[0] = gl_MultiTexCoord0;
-	
+
 	vec3 n1 = normalize(gl_NormalMatrix * gl_Normal);
 	vec4 tangent1 = vec4(tangent.x, tangent.y, tangent.z, 0);
 	//vec3 t1 = normalize(gl_NormalMatrix * tangent1);
